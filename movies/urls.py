7a1d1656ae674415cc5a4fd2692fa09a3b2a4ae5
seungtaeryu <dbtmdxo1992@naver.com>
2022-11-16 17:46:40 +0900

    create genres.json & actors.json

--- movies/urls.py
+++ movies/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('getmovie/', views.getMovie),
+    path('makefixtures/', views.makeFixtures),
     path('createmovie/', views.createMovie),
 ]
