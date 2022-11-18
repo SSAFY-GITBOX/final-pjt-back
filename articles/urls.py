ecd10dfb2c7bbc81beb80bfcbfbf04eef9fae087
becoding96 <becoding96@gmail.com>
2022-11-18 14:10:26 +0900

    update getting comment_list for article_detail

--- articles/urls.py
+++ articles/urls.py
@@ -6,9 +6,8 @@ from . import views
 urlpatterns = [
     path('', views.article_list),
     path('<int:article_pk>/', views.article_detail),
-    path('comments/', views.comment_list),
+    path('<int:article_pk>/comments/', views.comment_list),
     path('comments/<int:comment_pk>/', views.comment_detail),
-    path('<int:article_pk>/comments/', views.comment_create),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -45,12 +45,19 @@ def article_detail(request, article_pk):
             return Response(serializer.data)
 
 
-@api_view(['GET'])
-def comment_list(request):
+@api_view(['GET', 'POST'])
+def comment_list(request, article_pk):
     if request.method == 'GET':
-        comments = get_list_or_404(ArticleComment)
+        comments = get_list_or_404(ArticleComment, article_id=article_pk)
         serializer = CommentSerializer(comments, many=True)
         return Response(serializer.data)
+    
+    if request.method == 'POST':
+        article = get_object_or_404(Article, pk=article_pk)
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(article=article)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 @api_view(['GET', 'DELETE', 'PUT'])
@@ -70,12 +77,3 @@ def comment_detail(request, comment_pk):
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data)
-
-
-@api_view(['POST'])
-def comment_create(request, article_pk):
-    article = get_object_or_404(Article, pk=article_pk)
-    serializer = CommentSerializer(data=request.data)
-    if serializer.is_valid(raise_exception=True):
-        serializer.save(article=article)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
